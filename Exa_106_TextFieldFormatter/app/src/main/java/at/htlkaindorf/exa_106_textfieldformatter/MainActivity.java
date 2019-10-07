d7b12ae805ab00d6483b826ec2ed7e05b5c213da
eliaswil <wileld17@htl-kaindorf.ac.at>
2019-10-07 15:10:33 +0200

    Exa_106: init commit

---
+++ Exa_106_TextFieldFormatter/app/src/main/java/at/htlkaindorf/exa_106_textfieldformatter/MainActivity.java
@@ -0,0 +1,14 @@
+package at.htlkaindorf.exa_106_textfieldformatter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
