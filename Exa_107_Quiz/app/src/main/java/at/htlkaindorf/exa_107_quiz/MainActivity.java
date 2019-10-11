99b88b8cb611a39cafd05e606ed0c1cf586b5690
eliaswil <wileld17@htl-kaindorf.ac.at>
2019-10-11 09:21:08 +0200

    Exa_107: v1.0: initial commit

---
+++ Exa_107_Quiz/app/src/main/java/at/htlkaindorf/exa_107_quiz/MainActivity.java
@@ -0,0 +1,15 @@
+package at.htlkaindorf.exa_107_quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
