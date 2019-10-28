97858168955e956a38bcebb71dc474dd84436ada
eliaswil <wileld17@htl-kaindorf.ac.at>
2019-10-28 10:53:58 +0100

    Exa_108: v1.0: init commit

---
+++ Exa_108_NumberPuzzleGame/app/src/main/java/at/htlkaindorf/exa_108_numberpuzzlegame/MainActivity.java
@@ -0,0 +1,14 @@
+package at.htlkaindorf.exa_108_numberpuzzlegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
